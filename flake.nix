42302ee3ab8328c9356a3806d07a647e0e936751
czaplicki <dev dot czaplicki at icloud dot com>
2026-02-20 18:59:29 +0100

    updated flake to use flake parts and added deps

--- flake.nix
+++ flake.nix
@@ -3,27 +3,25 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nu-lint.url = "git+https://codeberg.org/wvhulle/nu-lint";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    systems = [ "x86_64-linux" "aarch64-linux" ];
-  in {
-    packages = nixpkgs.lib.genAttrs systems (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        mmw = pkgs.stdenvNoCC.mkDerivation {
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+      
+        packages.mmw = pkgs.stdenvNoCC.mkDerivation {
           pname = "mmw";
           version = "0.3.0";
-
           src = ./.;
 
-          buildInputs = [ pkgs.scdoc ];
+          nativeBuildInputs = [ pkgs.scdoc ];
 
           installPhase = ''
             mkdir -p $out/bin $out/share/man/man1
-
             install -m755 mmw.nu $out/bin/mmw
             scdoc < mmw.1.scd > $out/share/man/man1/mmw.1
           '';
@@ -32,27 +30,33 @@
             nushell
             imagemagick
             wlr-randr
+            xrandr
           ];
-        };
-        default = self.packages.${system}.mmw;
-      }
-    );
 
-    devShells = nixpkgs.lib.genAttrs systems (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        default = pkgs.mkShell {
-          buildInputs = with pkgs; [
+          meta = {
+            description = "Multi-Monitor Wallpapers script";
+            mainProgram = "mmw";
+            platforms = [ system ];
+          };
+        };
+
+        packages.default = self'.packages.mmw;
+
+        apps.default = {
+          type = "app";
+          program = pkgs.lib.getExe self'.packages.mmw;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
             scdoc
             nushell
+            inputs'.nu-lint.packages.default
             imagemagick
             wlr-randr
+            xrandr
           ];
         };
-      }
-    );
-
-
-  };
+      };
+    };
 }
